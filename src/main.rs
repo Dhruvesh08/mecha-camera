3549df9aedc9ef0228f3a0a27e45bdf07a5299c9
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-09 11:21:59 +0530

    video capture using pipeline

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,54 @@ fn main() -> Result<(), Error> {
     // Initialize GStreamer
     gstreamer::init()?;
 
-    // Build the pipeline
-    let pipeline = gstreamer::parse_launch("v4l2src ! videoconvert ! vpuenc_h264 !  filesink location=captured_video.h264")?;
+    // Create the elements
+    let source = gstreamer::ElementFactory::make("v4l2src").build()?;
+
+    let converter = gstreamer::ElementFactory::make("videoconvert").build()?;
+    let encoder = gstreamer::ElementFactory::make("vpuenc_h264").build()?;
+    let filesink = gstreamer::ElementFactory::make("filesink")
+        .property("location", "captured_video_v1.h264")
+        .build()?;
+    // Create the empty pipeline
+    let pipeline = gstreamer::Pipeline::new(Some("test-pipeline"));
+
+    // Add elements to the pipeline
+    pipeline.add_many(&[&source, &converter, &encoder, &filesink])?;
+
+    // Link the elements
+    gstreamer::Element::link_many(&[&source, &converter, &encoder, &filesink])?;
 
     // Start recording
-    let _ = pipeline.set_state(gstreamer::State::Playing)?;
+    pipeline.set_state(gstreamer::State::Playing)?;
 
     // Wait for the pipeline to be ready
     let bus = pipeline.bus().expect("Pipeline has no bus");
-    let msg = bus.timed_pop_filtered(gstreamer::ClockTime::NONE, &[gstreamer::MessageType::Eos, gstreamer::MessageType::Error]);
+    let msg = bus.timed_pop_filtered(
+        gstreamer::ClockTime::NONE,
+        &[gstreamer::MessageType::Eos, gstreamer::MessageType::Error],
+    );
     match msg {
-        Some(msg) => {
-            match msg.view() {
-                gstreamer::MessageView::Error(err) => {
-                    eprintln!(
-                        "Error from {:?}: {} ({:?})",
-                        err.src().map(|s| s.path_string()),
-                        err.error(),
-                        err.debug()
-                    );
-                }
-                gstreamer::MessageView::Eos(..) => {
-                    println!("Recording finished");
-                }
-                _ => unreachable!(),
+        Some(msg) => match msg.view() {
+            gstreamer::MessageView::Error(err) => {
+                eprintln!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
             }
-        }
+            gstreamer::MessageView::Eos(..) => {
+                println!("Recording finished");
+            }
+            _ => unreachable!(),
+        },
         None => {
             eprintln!("Failed to receive message from the bus");
         }
     }
 
     // Stop the pipeline
-    let _ = pipeline.set_state(gstreamer::State::Null)?;
+    pipeline.set_state(gstreamer::State::Null)?;
 
     Ok(())
 }
